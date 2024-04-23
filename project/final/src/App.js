a9825a5f064eee29f25465f15bba72e6f3e9279e
Theerdesh <88609511+Theerdesh@users.noreply.github.com>
2024-04-24 01:48:22 +0530

    changed app.js

--- project/final/src/App.js
+++ project/final/src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+
+import LeftBundleBranchBlock from './components/LeftBundleBranchBlock';
+import PrematureAtrialContraction from './components/PrematureAtrialContraction';
+import PrematureVentricularContraction from './components/PrematureVentricularContraction';
+import RightBundleBranchBlock from './components/RightBundleBranchBlock';
+import VentricularFibrillation from './components/VentricularFibrillation';
+
+import ECGUploader from './ECGUploader';
+
 import HeartDiseaseForm from './components/heart'; // Adjust the path as per your folder structure
+
+
 
 function App() {
   return (
